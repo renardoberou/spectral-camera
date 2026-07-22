e9bed12248cfbd33dc645a0184a974ad14f57d97
Bernardo Roustand <renardoberou@gmail.com>
2026-07-22 15:09:14 -0300

    fix(build): correct coroutines Guava dependency coordinate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.material:material:1.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx:kotlinx-coroutines-guava:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.11.0")
 
     testImplementation("junit:junit:4.13.2")
 
